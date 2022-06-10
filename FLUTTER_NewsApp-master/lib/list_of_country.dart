a1cf1e265e27a1c0970a68105991946603f7e860
Enes Aldemir <ealdemir42@hotmail.com>
2022-06-10 11:57:26 +0300

    v1.0

---
+++ FLUTTER_NewsApp-master/lib/list_of_country.dart
@@ -0,0 +1,32 @@
+const listOfCountry = [
+  {"name": "Hindistan", "code": "in"},
+  {"name": "Amerika", "code": "us"},
+  {"name": "Meksika", "code": "mx"},
+  {"name": "Birleşik Arap Emirlikleri", "code": "ae"},
+  {"name": "Yeni Zelanda", "code": "nz"},
+  {"name": "İsrail", "code": "il"},
+  {"name": "Endonezya", "code": "id"},
+];
+
+const listOfCatagory = [
+  {"name": "Bilim", "code": "science"},
+  {"name": "İş Haberleri", "code": "business"},
+  {"name": "Teknoloji", "code": "technology"},
+  {"name": "Spor", "code": "sports"},
+  {"name": "Sağlık", "code": "health"},
+  {"name": "Genel", "code": "general"},
+  {"name": "Magazin", "code": "entertainment"},
+  {"name": "Hepsi", "code": null},
+];
+const listOfNewsChannel = [
+  {"name": "BBC News", "code": "bbc-news"},
+  {"name": "The Times of India", "code": "the-times-of-india"},
+  {"code": "politico", "name": "politico"},
+  {"code": "the-washington-post", "name": "The Washington Post"},
+  {"code": "reuters", "name": "reuters"},
+  {"code": "cnn", "name": "cnn"},
+  {"code": "nbc-news", "name": "nbc news"},
+  {"code": "the-hill", "name": "The Hill"},
+  {"code": "fox-news", "name": "Fox News"},
+  {"code": "fox-news", "name": "Fox News"},
+];
